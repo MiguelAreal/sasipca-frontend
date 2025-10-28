84323d1de8e00d48e7ce4c7ea2f328c5e922c92d
Miguel MA. Areal <miguel.areal04@gmail.com>
2025-10-28 16:56:44 +0000

    Update ReceptionScreen.kt
    Correção de imports

--- composeApp/src/commonMain/kotlin/g8/ipca/sasipca/sasipca/screens/ReceptionScreen.kt
+++ composeApp/src/commonMain/kotlin/g8/ipca/sasipca/sasipca/screens/ReceptionScreen.kt
@@ -20,7 +20,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import g8.ipca.sasipca.sasipca.components.BarcodeInputField
+import g8.ipca.sasipca.sasipca.ui.components.BarcodeInputField
 import g8.ipca.sasipca.sasipca.ui.components.HeaderSection
 import g8.ipca.sasipca.sasipca.ui.utils.getFormattedDatePt
 
